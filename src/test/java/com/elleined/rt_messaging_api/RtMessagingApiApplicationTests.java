a139aa069a16f45845986a6f6c03f21579c9d10a
Elleined <demadegu@gmail.com>
2024-06-02 23:35:17 +0800

    Initial commit

---
+++ src/test/java/com/elleined/rt_messaging_api/RtMessagingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elleined.rt_messaging_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtMessagingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
