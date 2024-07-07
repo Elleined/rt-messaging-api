55db1e3c444baa90fa6a71dd3c18d9075ec85127
Elleined <demadegu@gmail.com>
2024-07-07 13:09:58 +0800

    Changes in pom and application properties

--- src/main/java/com/elleined/rt_messaging_api/RtMessagingApiApplication.java
+++ src/main/java/com/elleined/rt_messaging_api/RtMessagingApiApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableScheduling
-@EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class RtMessagingApiApplication {
 
 	public static void main(String[] args) {
