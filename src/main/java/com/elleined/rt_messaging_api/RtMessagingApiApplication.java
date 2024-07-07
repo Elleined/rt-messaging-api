f165d1db8739daf13dbc31fbfa8bc931acd7930b
Elleined <demadegu@gmail.com>
2024-07-07 22:41:54 +0800

    Added discovery client

--- src/main/java/com/elleined/rt_messaging_api/RtMessagingApiApplication.java
+++ src/main/java/com/elleined/rt_messaging_api/RtMessagingApiApplication.java
@@ -3,6 +3,7 @@ package com.elleined.rt_messaging_api;
 import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,8 +12,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableTransactionManagement
-@EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class RtMessagingApiApplication {
 
