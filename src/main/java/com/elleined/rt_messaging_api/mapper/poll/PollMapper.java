5fcef8a610024d8b79b21f60b1884ce1e32ed8d4
Elleined <demadegu@gmail.com>
2024-07-07 21:19:48 +0800

    Refactor mapper package

--- src/main/java/com/elleined/rt_messaging_api/mapper/poll/PollMapper.java
+++ src/main/java/com/elleined/rt_messaging_api/mapper/poll/PollMapper.java
@@ -2,6 +2,8 @@ package com.elleined.rt_messaging_api.mapper.poll;
 
 import com.elleined.rt_messaging_api.dto.poll.PollDTO;
 import com.elleined.rt_messaging_api.mapper.CustomMapper;
+import com.elleined.rt_messaging_api.mapper.chat.GroupChatMapper;
+import com.elleined.rt_messaging_api.mapper.user.UserMapper;
 import com.elleined.rt_messaging_api.model.chat.GroupChat;
 import com.elleined.rt_messaging_api.model.poll.Poll;
 import com.elleined.rt_messaging_api.model.user.User;
@@ -9,17 +11,22 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        uses = {
+                UserMapper.class,
+                GroupChatMapper.class
+        }
+)
 public interface PollMapper extends CustomMapper<Poll, PollDTO> {
 
     @Override
     @Mappings({
             @Mapping(target = "id", source = "id"),
             @Mapping(target = "createdAt", source = "createdAt"),
-            @Mapping(target = "creatorId", source = "creator.id"),
+            @Mapping(target = "creatorDTO", source = "creator"),
             @Mapping(target = "question", source = "question"),
-            @Mapping(target = "groupChatId", source = "groupChat.id"),
-            @Mapping(target = "optionIds", expression = "java(poll.optionIds())"),
+            @Mapping(target = "groupChatDTO", source = "groupChat")
     })
     PollDTO toDTO(Poll poll);
 
